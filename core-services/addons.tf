2478a2cb75df90a743bd633e9f4149110fe120a5
chrisghill <chris@massdriver.cloud>
2023-01-23 14:57:54 -0700

    spacing and comment

--- core-services/addons.tf
+++ core-services/addons.tf
@@ -1,8 +1,10 @@
 locals {
   enable_ebs_csi = true
 }
+
 module "ebs_csi" {
-  source              = "github.com/massdriver-cloud/terraform-modules//k8s/aws-ebs-csi-driver?ref=1961370"
+  source = "github.com/massdriver-cloud/terraform-modules//k8s/aws-ebs-csi-driver?ref=ffad03a"
+  // Using a count here in case we ever want to back this out to a conditional
   count               = local.enable_ebs_csi ? 1 : 0
   kubernetes_version  = var.k8s_version
   eks_cluster_arn     = data.aws_eks_cluster.cluster.arn
